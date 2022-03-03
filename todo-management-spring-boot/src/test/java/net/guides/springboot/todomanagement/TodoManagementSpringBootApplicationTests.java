aaf0b2274791b55726d885352acd1e5c382404f4
mdbrown543 <mdbrown543@gmail.com>
2022-03-02 22:29:30 -0600

    First commit

---
+++ todo-management-spring-boot/src/test/java/net/guides/springboot/todomanagement/TodoManagementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot.todomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
